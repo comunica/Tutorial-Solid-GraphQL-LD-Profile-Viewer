eafc366b25472782eb626dd3744e7ea097ae22f1
Ruben Taelman <ruben.taelman@ugent.be>
2019-09-10 15:06:40 +0200

    Add GraphQL-LD-based profile viewer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
+import { LoggedIn, LoggedOut, AuthButton, Value, List, withWebId } from '@solid/react';
+import { GraphQlLdProvider, Query } from 'solid-react-graphql-ld';
+import gql from "graphql-tag";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  state = { profileInput: '', activeProfile: '' };
+
+  componentDidUpdate(prevProps) {
+    const { webId } = this.props;
+    if (webId && webId !== prevProps.webId)
+      this.setState({ profileInput: webId });
+  }
+
+  viewProfile(profile) {
+    this.setState({ profileInput: profile, activeProfile: profile });
+  }
+
+  render() {
+    const { profileInput, activeProfile } = this.state;
+    return (
+      <div>
+        <h1>Profile viewer</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <LoggedOut>You are not logged in.</LoggedOut>
+          <LoggedIn>You are logged in as <Value src="user.name"/>.</LoggedIn>
+          <AuthButton popup="popup.html"/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          <label htmlFor="profile">Profile:</label>
+          <input id="profile" value={profileInput}
+                 onChange={e => this.setState({ profileInput: e.target.value })}/>
+          <button onClick={() => this.viewProfile(profileInput)}>View</button>
+        </p>
+
+        {activeProfile &&
+        <GraphQlLdProvider sources={[ activeProfile ]}>
+          <Query
+            query={gql`query @single(scope: all) {
+              name
+              image
+              friends @plural {
+                id
+                name @optional
+              }
+            }`}
+          >
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error: {error.toString()}</p>;
+              console.log(data);
+              return (
+                <dl>
+                  <dt>Full name</dt>
+                  <dd>{data.name}</dd>
+                  <dt>Image</dt>
+                  <dd><img src={data.image} alt={data.name} width="100px" /></dd>
+                  <dt>Friends</dt>
+                  <dd>
+                    <ul>
+                      {
+                        data.friends && data.friends.map(friend =>
+                          <li key={friend.id}>
+                            <button onClick={() => this.viewProfile(friend.id)}>
+                              {friend.name || friend.id}
+                            </button>
+                          </li>)
+                      }
+                    </ul>
+                  </dd>
+                </dl>
+              );
+            }}
+          </Query>
+        </GraphQlLdProvider>}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withWebId(App);
