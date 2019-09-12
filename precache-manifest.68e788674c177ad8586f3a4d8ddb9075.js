528d83a7e67cb6de569b0ef86ba5dd2397fc8b0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-12 06:44:46 +0000

    Deploy comunica/Tutorial-Solid-GraphQL-LD-Profile-Viewer to github.com/comunica/Tutorial-Solid-GraphQL-LD-Profile-Viewer.git:gh-pages

---
+++ precache-manifest.68e788674c177ad8586f3a4d8ddb9075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a612323512c32300f6010ee85c2a3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abbd59783ab8ad9d1314",
+    "url": "/static/css/main.24db9687.chunk.css"
+  },
+  {
+    "revision": "e3230cf589b921959201",
+    "url": "/static/js/2.1524472b.chunk.js"
+  },
+  {
+    "revision": "abbd59783ab8ad9d1314",
+    "url": "/static/js/main.aa0c26e0.chunk.js"
+  },
+  {
+    "revision": "635d0e032647e5def861",
+    "url": "/static/js/runtime~main.b42aab33.js"
+  }
+]);
